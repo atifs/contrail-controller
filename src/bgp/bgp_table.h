ec763cb87111a0ea1624a432c8dda9dae3b08188
Nischal Sheth <nsheth@juniper.net>
2014-03-18 22:33:10 -0700

    Make BgpTable::Export pure virtual.

--- src/bgp/bgp_table.h
+++ src/bgp/bgp_table.h
@@ -77,7 +77,7 @@ public:
 
     virtual bool Export(RibOut *ribout, Route *route,
                         const RibPeerSet &peerset,
-                        UpdateInfoSList &uinfo_slist);
+                        UpdateInfoSList &uinfo_slist) = 0;
 
     virtual Address::Family family() const = 0;
     virtual std::auto_ptr<DBEntry> AllocEntryStr(const std::string &key) const = 0;
